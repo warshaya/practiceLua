62ac416e48c6cd1c30808f866dbabf1d9e26035d
warshaya <aaron.warshay@gmail.com>
2017-09-17 16:22:00 -0400

    added line break for easier reading

--- eightQueen.lua
+++ eightQueen.lua
@@ -2,6 +2,7 @@ N = 8 -- board size
 
 found = 0
 count = 0
+
 -- check whether position (n,c) is free from attacks
 function isplaceok ( a, n, c )
   for i = 1, n - 1 do  -- for each queen already placed
